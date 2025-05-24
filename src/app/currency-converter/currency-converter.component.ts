696170b2a235e359e48d53289ec74b538c83c68c
Mojiabc <mojtaba.taghizadeh@abcfinlab.de>
2025-05-24 14:19:51 +0200

    fix: filtering the selected rate

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -1,21 +1,22 @@
-import { Component, inject, signal, computed,input,effect} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, NonNullableFormBuilder } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatOptionModule } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card';
-import { toSignal as rxToSignal } from '@angular/core/rxjs-interop';
+import {Component, inject, signal, computed, input, effect} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule, NonNullableFormBuilder} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatOptionModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {toSignal as rxToSignal} from '@angular/core/rxjs-interop';
 
 export interface CurrencyRate {
   base: { key: string; label: string };
   target: { key: string; label: string };
   baseToTargetRate: number;
 }
+
 @Component({
   selector: 'app-currency-converter',
-  imports: [ CommonModule,
+  imports: [CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
@@ -25,20 +26,34 @@ export interface CurrencyRate {
   templateUrl: './currency-converter.component.html',
   styleUrl: './currency-converter.component.scss'
 })
-export class CurrencyConverterComponent{
+export class CurrencyConverterComponent {
   rates = input<Array<CurrencyRate>>();
   uniqueCurrencies = computed(() => {
     const map = new Map<string, string>();
     if (!this.rates) {
       return [];
     }
-    for (const rate of this.rates()??[]) {
+    for (const rate of this.rates() ?? []) {
       map.set(rate.base.key, rate.base.label);
       map.set(rate.target.key, rate.target.label);
     }
-    return Array.from(map.entries()).map(([key, label]) => ({ key, label }));
+    return Array.from(map.entries()).map(([key, label]) => ({key, label}));
   });
   private fb = inject(NonNullableFormBuilder);
+
+  public get filteredBaseCurrencies() {
+    const target = this.form.controls.target.value;
+    return this.uniqueCurrencies().filter(
+      (c) => c.key !== target || c.key === this.form.value.base
+    );
+  };
+
+  public get filteredTargetCurrencies() {
+    const base = this.form.controls.base.value;
+    return this.uniqueCurrencies().filter(
+      (c) => c.key !== base || c.key === this.form.value.target
+    );
+  };
 
   form = this.fb.group({
     base: 'EUR' as string,
@@ -62,6 +77,7 @@ export class CurrencyConverterComponent{
     initialValue: this.form.controls.targetAmount.value,
   });
   activeField = signal<'base' | 'target'>('base');
+
   constructor() {
     effect(() => {
       const base = this.base();
@@ -73,17 +89,18 @@ export class CurrencyConverterComponent{
         const baseAmt = this.baseAmount();
         this.form.patchValue({
           targetAmount: +(baseAmt * rate).toFixed(4),
-        }, { emitEvent: false });
+        }, {emitEvent: false});
       }
 
       if (this.activeField() === 'target') {
         const targetAmt = this.targetAmount();
         this.form.patchValue({
           baseAmount: +(targetAmt / rate).toFixed(4),
-        }, { emitEvent: false });
+        }, {emitEvent: false});
       }
     });
   }
+
   private getRate(base: string, target: string): number | undefined {
     return (
       this.rates()?.find(
